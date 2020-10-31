2ac1d5e7c6e7c78085b5c203f6fd6d561ca9991b
Lazaros Ioannidis <larjohn@gmail.com>
2020-11-01 01:07:39 +0200

    Richer sparql query

--- src/shared/services/sparql.service.ts
+++ src/shared/services/sparql.service.ts
@@ -24,8 +24,125 @@ export class SparqlService {
 
   private generateResourceConstructQuery(resourceUri: string): ConstructQuery {
     return <ConstructQuery>this.parser.parse(
-      `CONSTRUCT {<${resourceUri}> ?p ?o. ?s1 ?p1 <${resourceUri}> . ?s2 <${resourceUri}> ?o2.} WHERE 
-        {{ <${resourceUri}> ?p ?o.} UNION { ?s1 ?p1 <${resourceUri}>.} UNION { ?s2 <${resourceUri}> ?o2.}} `,
+      `
+PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+CONSTRUCT {
+  <${resourceUri}> ?p ?o. 
+  ?o ?bnp ?bnv.
+  ?s1 ?p1 <${resourceUri}> . 
+  ?s2 <${resourceUri}> ?o2. 
+  
+  ?actual rdf:first ?first.
+  ?actual rdf:rest ?rest.
+  ?first ?elp ?val.
+  ?oo ?lp2 ?list.
+ <${resourceUri}> ?lp ?list.
+ 
+ 
+  ?p ?pLabelProperty ?pLabel.
+  ?o ?oLabelProperty ?oLabel.
+  ?bnp ?bnpLabelProperty ?bnpLabel.
+  ?bnv ?bnvLabelProperty ?bnvLabel.
+  ?p1 ?p1LabelProperty ?p1Label.
+  ?s1 ?s1LabelProperty ?s1Label.
+  ?s2 ?s2LabelProperty ?s2Label.
+  ?o2 ?o2LabelProperty ?o2Label.
+  ?elp ?elpLabelProperty ?elpLabel.
+  ?val ?valLabelProperty ?valLabel.
+
+} WHERE {
+  { 
+    <${resourceUri}> ?p ?o.
+    OPTIONAL {
+      FILTER (isBlank(?o)) .
+      ?o ?bnp ?bnv.
+      
+      OPTIONAL {
+        ?bnp ?bnpLabelProperty ?bnpLabel.
+        ?bnpLabelProperty rdfs:subPropertyOf* rdfs:label
+      }
+      
+          
+      OPTIONAL {
+        ?bnv ?bnvLabelProperty ?bnvLabel.
+        ?bnvLabelProperty rdfs:subPropertyOf* rdfs:label
+      }
+    }
+    
+    OPTIONAL {
+      ?p ?pLabelProperty ?pLabel.
+      ?pLabelProperty rdfs:subPropertyOf* rdfs:label
+    }
+    
+        
+    OPTIONAL {
+      ?o ?oLabelProperty ?oLabel.
+      ?oLabelProperty rdfs:subPropertyOf* rdfs:label
+    }
+        
+
+    
+  } 
+  UNION { 
+    ?s1 ?p1 <${resourceUri}>.
+    
+    OPTIONAL {
+      ?p1 ?p1LabelProperty ?p1Label.
+      ?p1LabelProperty rdfs:subPropertyOf* rdfs:label
+    }
+    
+        
+    OPTIONAL {
+      ?s1 ?s1LabelProperty ?s1Label.
+      ?s1LabelProperty rdfs:subPropertyOf* rdfs:label
+    }
+     
+    
+  } 
+  UNION { 
+    ?s2 <${resourceUri}> ?o2.
+    
+    OPTIONAL {
+      ?s2 ?s2LabelProperty ?s2Label.
+      ?s2LabelProperty rdfs:subPropertyOf* rdfs:label
+    }
+    
+        
+    OPTIONAL {
+      ?o2 ?o2LabelProperty ?o2Label.
+      ?o2LabelProperty rdfs:subPropertyOf* rdfs:label
+    }
+     
+  }
+  UNION {
+    { 
+      <${resourceUri}> ?lp ?list.
+    } UNION {
+    
+      <${resourceUri}> ?op ?oo.
+      ?oo ?lp2 ?list.
+
+    }
+    ?list (rdf:rest*)/rdf:first ?first.
+    ?actual rdf:first ?first.
+    OPTIONAL {?actual rdf:rest ?rest.}
+    ?first ?elp ?val.  
+    
+    OPTIONAL {
+      ?elp ?elpLabelProperty ?elpLabel.
+      ?elpLabelProperty rdfs:subPropertyOf* rdfs:label
+    }
+    
+        
+    OPTIONAL {
+      ?val ?valLabelProperty ?valLabel.
+      ?valLabelProperty rdfs:subPropertyOf* rdfs:label
+    }
+     
+    
+  }  
+} `
     );
   }
 
